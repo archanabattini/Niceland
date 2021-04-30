81894655c0eafe8193b5c6b209ed5ab5ee859bd7
Archana <archana.battini@gmail.com>
2021-04-30 17:16:05 +0100

    Item Model and its tests.

--- Niceland Inventory.Tests/Models/ItemTest.cs
+++ Niceland Inventory.Tests/Models/ItemTest.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using Niceland_Inventory.Models;
+using NUnit.Framework;
 using System;
 
 namespace Niceland_Inventory.Tests.Models
@@ -9,81 +10,75 @@ namespace Niceland_Inventory.Tests.Models
         [Test]
         public void Name()
         {
-            Assert.Throws(new Item()); //Default constructor not allowed.
-
             Item item = new Item("Aged Brie");
-            Assert.Equals("Aged Brie", item.Name);//Success
+            Assert.AreEqual("Aged Brie", item.Name);//Success
 
             Item item2 = new Item("Christmas Crackers");
-            Assert.Equals("Christmas Crackers", item2.Name);
+            Assert.AreEqual("Christmas Crackers", item2.Name);
             item2.Name = "Soap";
-            Assert.Equals("Soap", item6.Name);// Name is changeable
+            Assert.AreEqual("Soap", item2.Name);// Name is changeable
 
-            Assert.Throws<NotSupportedException>(() => item6.Name = "");//Name cannot be set to Empty.
+            Assert.Throws<NotSupportedException>(() => item2.Name = "");//Name cannot be set to Empty.
 
-            Assert.Throws<NotSupportedException>(new Item(""));//Item cannot be created with empty name.
+            Assert.Throws<NotSupportedException>(() => new Item(""));//Item cannot be created with empty name.
         }
 
         [Test]
         public void SellValue()
         {
-            Assert.Throws(new Item());//Default constructor not allowed.
-
             Item item = new Item("Aged Brie");
-            Assert.Equals(0, item.SellValue);//SellValue set by default to zero.
+            Assert.AreEqual(0, item.SellValue);//SellValue set by default to zero.
             Assert.True(item.SellValue is int);//SellValue must be integer
 
             Item item2 = new Item("Christmas Crackers");
-            Assert.Equals(0, item2.SellValue);
+            Assert.AreEqual(0, item2.SellValue);
             item2.SellValue = -99;
-            Assert.Equals(-99, item2.SellValue);//SellValue can be negative.
+            Assert.AreEqual(-99, item2.SellValue);//SellValue can be negative.
             item2.SellValue = 5;
-            Assert.Equals(5, item2.SellValue);//SellValue is positive Integer.
+            Assert.AreEqual(5, item2.SellValue);//SellValue is positive Integer.
 
             Item item3 = new Item("Fresh Item");
-            Assert.Equals("Fresh Item", item3.Name);//Success
+            Assert.AreEqual("Fresh Item", item3.Name);//Success
             item3.SellValue = 9;
-            Assert.Equals(9, item3.SellValue);
+            Assert.AreEqual(9, item3.SellValue);
             item3.Name = "Soap";
-            Assert.Equals("Soap", item3.Name);
-            Assert.Equals(0, item3.SellValue);//When Name changed, SellValue will be reset to zero.
+            Assert.AreEqual("Soap", item3.Name);
+            Assert.AreEqual(0, item3.SellValue);//When Name changed, SellValue will be reset to zero.
 
-            Assert.Throws<NotSupportedException>(new Item(""));//Item cannot be created with empty name.
+            Assert.Throws<NotSupportedException>(() => new Item(""));//Item cannot be created with empty name.
         }
 
         [Test]
         public void QualityValue()
         {
-            Assert.Throws(new Item());//Default constructor not allowed.
-
             Item item = new Item("Aged Brie");
-            Assert.Equals(0, item.QualityValue);//QualityValue set by default to zero.
+            Assert.AreEqual(0, item.QualityValue);//QualityValue set by default to zero.
             Assert.True(item.QualityValue is int);//QualityValue must be integer
 
             Item item2 = new Item("Christmas Crackers");
-            Assert.Equals(0, item2.QualityValue);
+            Assert.AreEqual(0, item2.QualityValue);
             item2.QualityValue = -99;
-            Assert.Equals(0, item2.QualityValue);//QualityValue cannot be negative.
+            Assert.AreEqual(0, item2.QualityValue);//QualityValue cannot be negative.
             item2.QualityValue = -1;
-            Assert.Equals(0, item2.QualityValue);//QualityValue boundary condition - overflow
+            Assert.AreEqual(0, item2.QualityValue);//QualityValue boundary condition - overflow
             item2.QualityValue = 0;
-            Assert.Equals(0, item2.QualityValue);//QualityValue boundary condition - min
+            Assert.AreEqual(0, item2.QualityValue);//QualityValue boundary condition - min
             item2.QualityValue = 5;
-            Assert.Equals(5, item2.QualityValue);//QualityValue appropriate
+            Assert.AreEqual(5, item2.QualityValue);//QualityValue appropriate
             item2.QualityValue = 50;
-            Assert.Equals(50, item2.QualityValue);//QualityValue boundary condition - max
+            Assert.AreEqual(50, item2.QualityValue);//QualityValue boundary condition - max
             item2.QualityValue = 51;
-            Assert.Equals(50, item2.QualityValue);//QualityValue boundary condition - overflow
+            Assert.AreEqual(50, item2.QualityValue);//QualityValue boundary condition - overflow
 
             Item item3 = new Item("Fresh Item");
-            Assert.Equals("Fresh Item", item3.Name);//Success
+            Assert.AreEqual("Fresh Item", item3.Name);//Success
             item3.QualityValue = 9;
-            Assert.Equals(9, item3.QualityValue);
+            Assert.AreEqual(9, item3.QualityValue);
             item3.Name = "Soap";
-            Assert.Equals("Soap", item3.Name);
-            Assert.Equals(0, item3.QualityValue);//When Name changed QualityValue reset to zero.
+            Assert.AreEqual("Soap", item3.Name);
+            Assert.AreEqual(0, item3.QualityValue);//When Name changed QualityValue reset to zero.
 
-            Assert.Throws<NotSupportedException>(new Item(""));//Item cannot be created with empty name.
+            Assert.Throws<NotSupportedException>(() => new Item(""));//Item cannot be created with empty name.
         }
     }
 }
---
+++ Niceland Inventory/Models/Item.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Niceland_Inventory.Models
+{
+    // Inventory Management - Item
+    public class Item
+    {
+        private string name;
+        private int sellValue;
+        private int qualityValue;
+
+        private Item() { }//No Default constructor
+        public Item(string name)
+        {
+            Name = name;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set {
+                if (value == null || value.Length == 0)
+                {
+                    throw new NotSupportedException();
+                }
+                name = value;
+                SellValue = 0;
+                QualityValue = 0;
+            }
+        }
+        public int SellValue
+        {
+            get { return sellValue; }
+            set { sellValue = value; }
+        }
+
+        public int QualityValue
+        {
+            get { return qualityValue; }
+            set
+            {
+                if (value < 0)
+                {
+                    qualityValue = 0;
+                }
+                else if (value > 50)
+                {
+                    qualityValue = 50;
+                }
+                else
+                {
+                    qualityValue = value;
+                }
+
+            }
+        }
+    }
+}
